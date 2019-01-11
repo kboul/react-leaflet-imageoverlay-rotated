8e4f277c5025d4e8b45aafc5455aa447ca8ad0ee
kboul <kos.voulgaridis@gmail.com>
2019-01-11 16:29:10 +0200

    Import leaflet.css on index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'leaflet/dist/leaflet.css';
 import './index.css';
-import App from './App';
+
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
