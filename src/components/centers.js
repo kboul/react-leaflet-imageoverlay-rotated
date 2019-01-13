dc6d9e64a176d0654837fffd574f78b44f814f69
kboul <kos.voulgaridis@gmail.com>
2019-01-13 23:39:58 +0200

    Add moscow coords to centers.js

--- src/components/centers.js
+++ src/components/centers.js
@@ -5,4 +5,5 @@ export const centers = [
     L.latLng(41.894802, 12.4853384), // rome,
     L.latLng(40.4167047, -3.7035825), // madrid,
     L.latLng(34.0536834, -118.2427669), // madrid
+    L.latLng(55.7507178, 37.6176606), // moscow
 ]
