937d6761aea91c7be9e23af241b135b49d8dfacb
kboul <kos.voulgaridis@gmail.com>
2019-01-13 18:20:31 +0200

    Create an imagesUrls reusable file

---
+++ src/components/imagesUrls.js
@@ -0,0 +1,6 @@
+export const imagesUrls = [
+    "https://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg",
+    "https://pbs.twimg.com/profile_images/492243151755542528/SczvOopQ_400x400.jpeg",
+    "https://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg",
+    "https://www.thetimes.co.uk/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2Ffd88d450-fb0b-11e7-9a34-94e1b34681c3.jpg?crop=5314%2C2989%2C0%2C277&resize=685"
+];
