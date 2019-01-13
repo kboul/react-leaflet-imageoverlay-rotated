dace702ba303331b5a5580397f607613ad8efacf
kboul <kos.voulgaridis@gmail.com>
2019-01-13 18:25:17 +0200

    Update imageUrls.js

--- src/components/imagesUrls.js
+++ src/components/imagesUrls.js
@@ -1,6 +1,7 @@
 export const imagesUrls = [
     "https://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg",
     "https://pbs.twimg.com/profile_images/492243151755542528/SczvOopQ_400x400.jpeg",
-    "https://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg",
+    "https://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/557234-istock-503488234.jpg?itok=empSJXDv&resize=1100x1100",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Gatto_europeo4.jpg/250px-Gatto_europeo4.jpg",
     "https://www.thetimes.co.uk/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2Ffd88d450-fb0b-11e7-9a34-94e1b34681c3.jpg?crop=5314%2C2989%2C0%2C277&resize=685"
 ];
